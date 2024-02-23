39f54134d2b743fc980369df214f589cf46c1b67
Mihan3000 <121653576+Mihan3000@users.noreply.github.com>
2024-02-23 17:56:27 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -64,7 +64,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     
 	# R11
 	export FOX_R11=2
-	export FOX_VERSION="android-13"
+	export FOX_VERSION="12.1"
 	export FOX_VARIANT=MIUI
 	export OF_PATCH_AVB20=1
 	export OF_MAINTAINER="Mihan3000"
