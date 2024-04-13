3fe3cc60549f94010e1404c635c019768b0d542d
Mihan3000 <121653576+Mihan3000@users.noreply.github.com>
2024-04-13 15:52:29 +0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,87 +14,71 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/xiaomi/chopin
-
 # API
 PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # A/B
 ENABLE_VIRTUAL_AB := true
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
 AB_OTA_UPDATER := true
-
 AB_OTA_PARTITIONS += \
     boot \
     dtbo \
-    lk \
-    preloader \
-    odm \
-    odm_dlkm \
-    product \
     system \
     system_ext \
-    vbmeta_system \
-    vbemeta_vendor \
+    product \
     vendor \
-    vendor_dlkm \
-    vendor_boot
+    odm \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor
 
 PRODUCT_PACKAGES += \
-    otapreopt_script \
     update_engine \
     update_engine_sideload \
     update_verifier
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl.recovery \
-    android.hardware.boot@1.1-impl
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-mtkimpl \
+    libmtk_bsg.recovery
+    
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
-
-# Heath hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
     
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Additional target Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1 \
     libkeymaster4 \
     libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so
-
-# Keystore2
-PRODUCT_PACKAGES += \
-    android.system.keystore2
-
-# Mtk plpath utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
-
-# Otacert
-#PRODUCT_EXTRA_RECOVERY_KEYS += \
-#    $(DEVICE_PATH)/security/miui_releasekey
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
